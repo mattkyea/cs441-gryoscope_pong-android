dff5d72f2611c95bc538dc487bd5d8d45a40bdfe
mattkyea <kyeamatt@gmail.com>
2021-03-24 10:06:41 -0400

    added libGDX files

---
+++ Android/android/src/com/gryoscopepong/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.gryoscopepong;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gryoscopepong.GryoscopePong;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GryoscopePong(), config);
+	}
+}
